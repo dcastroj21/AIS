beeeb884e7fb2c8be5232cca9747325f7ba3aae2
Steven <stevenllerena@hotmail.com>
2017-05-01 00:30:25 -0500

    f

--- MySQL/mmsi.php
+++ MySQL/mmsi.php
@@ -5,7 +5,10 @@ $tabla=array();
 $i=0;
 
 
-$consulta=mysql_query("SELECT id,nombre,mmsi,status,latitud,longitud,velocidad,curso,max(fecha) FROM datos GROUP BY mmsi") or die("Problemas en consulta: ".mysql_error());
+// $consulta=mysql_query("SELECT id,nombre,mmsi,status,latitud,longitud,velocidad,curso,max(fecha) FROM datos GROUP BY mmsi") or die("Problemas en consulta: ".mysql_error());
+$consulta=mysql_query("Select T1.* From datos As T1 Inner Join (Select mmsi, Max(fecha) As Max_Fecha From datos Group By mmsi) As T2 On T1.mmsi = T2.mmsi And T1.fecha = T2.Max_Fecha") or die("Problemas en consulta: ".mysql_error());
+
+
 
 // Select id,mmsi,status,latitud,max(fecha) From datos Group By mmsi
     while($reg=mysql_fetch_array($consulta)){
